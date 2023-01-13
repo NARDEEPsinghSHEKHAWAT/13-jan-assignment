a798caff3e448fea6c7af90123193611cf938f96
Torpedo_Ded <94340453+NARDEEPsinghSHEKHAWAT@users.noreply.github.com>
2023-01-13 16:43:24 +0530

    Update problem 2.cpp

--- problem 2.cpp
+++ problem 2.cpp
@@ -1,35 +1,24 @@
-// Include header file
-#include <iostream>
-#include <string>
-#include <vector>
-
-class tri
-{
-    public:
-    std::string ss;
-    std::string name;
-    tri(std::string ss)
-    {
-        name = ss;
-    }
-    tri()
-    {
-        name = "I love programming languages ";
-    }
-};
+Q2
+//class programming
+#include<iostream>
+using namespace std;
 class Programming
 {
-    public:
-    static void main(std::vector<std::string> &args)
-    {
-        tri* obj = new tri();
-        obj->ss = "Java";
-        std::cout << "I love " + obj->ss << std::endl;
-        std::cout << obj->name << std::endl;
-    }
+	public:
+		Programming(char arr[])
+		{
+			cout<<"I love "<<arr<<endl;
+		}
+		Programming()
+		{
+			cout<<"I love programming"<<endl;
+		}
 };
-int main(int argc, char **argv){
-	std::vector<std::string> parameter(argv + 1, argv + argc);
-	Programming::main(parameter); 
-	return 0;
-};
+int main()
+{
+	char str[10];
+	cout<<"enter string to be printed\n";
+	cin>>str;
+	Programming P1;
+	Programming P2(str);
+}
